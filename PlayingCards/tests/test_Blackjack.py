b861d3d137c59fe4153158784883c7e6fc0f119d
Vince989 <vince989@hotmail.com>
2022-01-15 08:52:42 -0500

    Minor doc/comment improvements, and added file, requirements.txt

--- PlayingCards/tests/test_Blackjack.py
+++ PlayingCards/tests/test_Blackjack.py
@@ -13,14 +13,14 @@ def test_blackjack():
 
 
 def test_eval_score():
-    player = PlayingCardsPlayer("TEST")
+    player = PlayingCardsPlayer("TEST")  # Arbitrary name
     player.hand.add(make_2_of_diamonds())
     player.hand.add(make_ace_of_spades())
     assert Blackjack.eval_score(player.hand) == 13  # 2 + 11
     player.hand.add(make_10_of_clubs())
     assert Blackjack.eval_score(player.hand) == 13  # 2 + 11 + 10 ... wait, 2 + 1 + 10
 
-    player = PlayingCardsPlayer("TEST2")
+    player = PlayingCardsPlayer("TEST2")  # Arbitrary name, again
     player.hand.add(make_ace_of_spades())
     player.hand.add(make_queen_of_hearts())
     assert Blackjack.eval_score(player.hand) == 21  # Blackjack case
