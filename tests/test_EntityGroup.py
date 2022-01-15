564a439640be2943c332162701a52a55cbdebb17
Vince989 <vince989@hotmail.com>
2022-01-15 08:05:44 -0500

    Unit tests for Take() and Add()

--- tests/test_EntityGroup.py
+++ tests/test_EntityGroup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from ..EntityGroup import EntityGroup
+from ..Entity import Entity
 
 
 # noinspection PyPep8Naming
@@ -9,4 +10,24 @@ def test_EntityGroup():
 
     assert entity_group.shuffle() is None  # Pass-through should work but return nothing
 
-    # TODO Test take() and add()
+
+def test_add():
+    entity_group = EntityGroup()
+    amount = 10
+    for i in range(amount):  # Generate 10 items
+        new_entity = Entity()
+        entity_group.add(new_entity)
+    assert len(entity_group.items) == amount
+
+
+def test_take():
+    entity_group = EntityGroup()
+    amount = 10
+    for i in range(amount):  # Generate 10 items
+        new_entity = Entity()
+        entity_group.add(new_entity)
+
+    taken = []
+    for j in range(3):
+        taken.append(entity_group.take())
+    assert len(taken) == 3
