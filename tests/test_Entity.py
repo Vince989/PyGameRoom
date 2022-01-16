75936fac46247e9f96ecb3f393021a5d90639b90
Vince989 <vince989@hotmail.com>
2022-01-16 09:40:38 -0500

    (Very) Basic unit tests for Entity and Player

--- tests/test_Entity.py
+++ tests/test_Entity.py
@@ -5,4 +5,4 @@ from ..Entity import Entity
 # noinspection PyPep8Naming
 def test_EntityInit():
     entity = Entity()
-    assert entity
+    assert entity  # TODO Improve
---
+++ tests/test_Player.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from Player import Player
+
+
+# noinspection PyPep8Naming
+def test_Player():
+    player = Player("THIS IS A TEST")
+    assert player  # TODO Improve
